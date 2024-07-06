f26c7d07ef6263dc03a77d231cc15b06bfe2c886
danishofcodes <161294358+danishofcodes@users.noreply.github.com>
2024-07-06 15:53:12 +0530

    Update Searchbar.jsx

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,7 +1,14 @@
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
-export default function Searchbar() {
+export default function Searchbar({searchWord, word}) {
+  
   return (
-    <input className='searchbar' type='text' placeholder='Search your word!'/>
+    <>
+    <label htmlFor="searchbar"></label>
+    <input className='searchbar' type='text' placeholder='Search your word!' value={word} onChange={searchWord}/>
+    </>
+  
   )
 }
