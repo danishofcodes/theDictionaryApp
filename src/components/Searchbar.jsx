cdbe18690a4fd8579793c39cc1ed9a3e1a4f32de
danishofcodes <161294358+danishofcodes@users.noreply.github.com>
2024-08-23 13:01:06 +0530

    changes

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -3,10 +3,15 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
 export default function Searchbar({onSearchWordChange, word, handleSearch}) {
-  
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
   return (
     <div className='searchbar-container'>
-    <input className='searchbar' type='text' placeholder='Search your word!' value={word} onChange={onSearchWordChange}/>
+    <input className='searchbar' type='text' placeholder='Search your word!' value={word} onChange={onSearchWordChange}   onKeyDown={handleKeyPress}/>
     <button className="search-button" onClick={handleSearch}><FontAwesomeIcon icon={faSearch}/></button>
     </div>
   
